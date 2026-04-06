c33389579dd6c1a57bc6c5c51e2cf6e6d6514720
katarinablu-u <dodorim55@skuniv.ac.kr>
2026-04-06 17:23:15 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int A;
+    int B;
+    cin>> A>>B;
+    cout<<A+B;
+
+    return 0;
+}
+
