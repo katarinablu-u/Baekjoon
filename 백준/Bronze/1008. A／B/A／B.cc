a3de2a4f285d401967be03f5e309df0e1227b10f
katarinablu-u <dodorim55@skuniv.ac.kr>
2026-04-06 23:11:08 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    double a, b;
+    cin >> a >> b;
+
+    cout.precision(12); 
+    cout << fixed;
+
+    cout << a / b << "\n";
+
+    return 0;
+}
